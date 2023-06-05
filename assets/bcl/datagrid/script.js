10138eb271ed9e334180f85bc8e0f452a9daef25
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-05 17:19:04 +0200

    bugfix and refactoring code

--- assets/bcl/datagrid/script.js
+++ assets/bcl/datagrid/script.js
@@ -12,20 +12,15 @@ BclDataGrid =
 {
     init : function()
     {
-        $('.bcl-datagrid').parent().on('click tap','.row',function(){
-            if (!$(this).data('url-detail')) {
-                return;
-            }
-            Osynapsy.History.save();
-            window.location = $(this).data('url-detail');            
-        }).on('click','.bcl-datagrid-th-order-by',function(){
-            if (!$(this).data('idx')) {
+        Osynapsy.element('body').on('click','.bcl-datagrid-th-order-by', function(){            
+            if (!this.dataset.idx) {
                 return;
             }            
-            var gridId = $(this).closest('.bcl-datagrid').attr('id');
-            var orderByField = $('.BclPaginationOrderBy','#'+gridId);
-            var orderByString = orderByField.val();
-            var curColumnIdx = $(this).data('idx');
+            let grid = this.closest('.bcl-datagrid');
+            let gridId = grid.getAttribute('id');
+            let orderByField = grid.querySelector('.bcl-paginator-order-by');
+            let orderByString = orderByField.value;
+            let curColumnIdx = this.dataset.idx;
             if (orderByString.indexOf('[' + curColumnIdx +']') > -1){
                 orderByString = orderByString.replace('[' + curColumnIdx + ']','[' + curColumnIdx + ' DESC]');                
             } else if (orderByString.indexOf('[' + curColumnIdx +' DESC]') > -1) {
@@ -33,10 +28,11 @@ BclDataGrid =
             } else {
                 orderByString += '[' + curColumnIdx + ']';                
             }
-            $('.BclPaginationCurrentPage','#'+gridId).val(1);
-            orderByField.val(orderByString);
+            grid.querySelector('.bcl-paginator-current-page').value = '1';
+            orderByField.value = orderByString;
             Osynapsy.refreshComponents([gridId]);
-        }).on('click','.bcl-datagrid-th-check-all', function(){
+        });
+        Osynapsy.element('body').on('click','.bcl-datagrid-th-check-all', function(){
             var className = $(this).data('fieldClass');
             $('.'+className).click();
         });
